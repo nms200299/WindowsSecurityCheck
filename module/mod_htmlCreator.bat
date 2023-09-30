1c98a468589dd3f7e9ac49b3c6707c24cf008d96
nms200299 <nms200299@naver.com>
2023-10-01 00:55:20 +0900

    html 파일 출력

--- module/mod_htmlCreator.bat
+++ module/mod_htmlCreator.bat
@@ -1,5 +1,19 @@
-:: 주석
-
 @echo off
-echo %1
+:: echo %1
+
+
+echo ^<!DOCTYPE HTML^> > test.html
+echo ^<html^> >> test.html
+echo 	^<head^> >> test.html
+echo 	^<title^>example^</title^> >> test.html
+echo 	^</head^> >> test.html
+echo. >> test.html
+echo 	^<body^> >> test.html
+
+echo 	^</body^> >> test.html
+echo ^</html^> >> test.html
+
+
+
+echo asd
 pause>nul
