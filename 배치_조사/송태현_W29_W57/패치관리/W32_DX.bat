f8ef1569570d8745a5c3d94c07e448faf38cf3f1
songtaehyeon <55786186+songtaehyeon@users.noreply.github.com>
2023-10-10 15:27:40 +0900

    Add files via upload

---
+++ 배치_조사/송태현_W29_W57/패치관리/W32_DX.bat
@@ -0,0 +1,8 @@
+@echo off
+echo W-32 START
+echo.                                                                                         
+echo [W32] 최신 HOT FIX 적용
+echo.
+echo ■ 진단 현황
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"
+echo ■ 결과 : 안전
---
+++ 배치_조사/송태현_W29_W57/패치관리/W33_DX.bat
@@ -0,0 +1,8 @@
+@echo off
+echo W-33 START
+echo.                                                                                         
+echo [W33] 백신 프로그램 업데이트                                                              
+echo.
+echo ■ 진단 현황                                                                                         
+net start | findstr /I "AlYac Hauri V3 test Symantec AVG"
+echo ■ 결과 : 애매                                                     
