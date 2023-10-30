cd686df89961a1b8244b94969ff9ed249457bea6
gPtjs0420 <134833277+gPtjs0420@users.noreply.github.com>
2023-10-30 22:53:37 +0900

    Add files via upload

--- 배치_조사/조혜선_W58_W82/패치관리/69.bat
+++ 배치_조사/조혜선_W58_W82/패치관리/69.bat
@@ -1,18 +1,23 @@
 @echo off
 
-echo [w69] 정책에 따른 시스템 로깅 설정                                                    
-
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo [ W-69 "정책에 따른 시스템 로깅 설정" ]                                                             >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditLogonEvents"             >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditPrivilegeUse"            >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditPolicyChange"            >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditAccountManage"           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditDSAccess"                >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditAccountLogon"            >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo W-69 END                                                                                   >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo ======================================================================================     >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
+echo. W-69 정책에 따른 시스템 로깅 설정
+echo.
+echo [ W-69 "정책에 따른 시스템 로깅 설정" ]
+echo.
+type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditLogonEvents"
+type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditPrivilegeUse"
+type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditPolicyChange"
+type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditAccountManage"
+type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditDSAccess"
+type [RESULT]_%COMPUTERNAME%_Local_Security_Policy.txt | Find /I "AuditAccountLogon"
+echo.
+if errorlevel 1 (
+    echo ■ 결과: 양호
+    
+) else (
+    echo ■ 결과: 취약
+    echo ■ 조치 방안
+    echo Step Step 1 시작> 실행> SECPOL.MSC> 로컬 정책> 감사 정책
+    echo 로그온 이벤트, 계정 로그온 이벤트, 정책 변경 : 성공/실패 감사
+    echo 계정 관리, 디렉토리 서비스 액세스, 권한 사용 : 실패 감사
+)
