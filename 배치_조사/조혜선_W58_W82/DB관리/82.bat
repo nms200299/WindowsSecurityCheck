2916f2ec4f9043096a98fdda6f8987f6cbfd6359
gPtjs0420 <134833277+gPtjs0420@users.noreply.github.com>
2023-10-30 22:43:32 +0900

    Add files via upload

--- 배치_조사/조혜선_W58_W82/DB관리/82.bat
+++ 배치_조사/조혜선_W58_W82/DB관리/82.bat
@@ -1,19 +1,24 @@
 @echo off
 
-echo [w82] Windows 인증 모드 사용                                                   
-
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo [ W-82 "Windows 인증 모드 사용" ]                                                             	>>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo  ■ MS-SQL 서비스 확인 ■                                          						        >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-net start | find /I "SQL Server"																>>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo  ■ MS-SQL 인증 모드 확인 ■                                          						    >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
+echo. W-82 Windows 인증 모드 사용
+echo.
+echo [ W-82 "Windows 인증 모드 사용" ]
+echo.
+echo  ■ MS-SQL 서비스 확인 ■                                          						       
+net start | find /I "SQL Server"																
+echo.                                                                                           
+echo.                                                                                           
+echo  ■ MS-SQL 인증 모드 확인 ■                                          						    
 reg export "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server" mssqlreg_%COMPUTERNAME%_WINSVR.dmp
-TYPE mssqlreg_%COMPUTERNAME%.dmp | findstr "LoginMode"											>>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-reg query "HKLM\Software\Microsoft\Microsoft SQL Server" /s | findstr /i "LoginMode"			>>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo W-82 END
+TYPE mssqlreg_%COMPUTERNAME%.dmp | findstr "LoginMode"											
+echo.                                                                                           
+reg query "HKLM\Software\Microsoft\Microsoft SQL Server" /s | findstr /i "LoginMode"
+echo.
+if errorlevel 1 (
+    echo ■ 결과: 양호
+    
+) else (
+    echo ■ 결과: 취약
+    echo ■ 조치 방안
+    echo Step 1 SQL Server Management Studio - 해당 서버 우클릭 - 속성 - 보안 - 서버 인증 - Windows 인증 모드'W'를 클릭하여 활성화
+)
