fef67a7a8fb9b6b33b2412373216f87f192ddf10
gPtjs0420 <134833277+gPtjs0420@users.noreply.github.com>
2023-10-30 22:48:17 +0900

    Add files via upload

--- 배치_조사/조혜선_W58_W82/로그관리/70.bat
+++ 배치_조사/조혜선_W58_W82/로그관리/70.bat
@@ -1,21 +1,25 @@
 @echo off
 
-echo [w70] 이벤트 로그 관리 설정                                                    
-
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo [ W-70 "이벤트 로그 관리 설정" ]                                                                  >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo. # 응용프로그램 확인 #                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Application" | find /I "MaxSize"     >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Application" | find /I "Retention"   >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-echo. # 시스템 확인 #                                                                               >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\System" | find /I "MaxSize"          >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\System" | find /I "Retention"        >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-echo. # 보안 확인 #                                                                                >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Security" | find /I "MaxSize"        >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Security" | find /I "Retention"      >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo W-70 END                                                                                   >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo ======================================================================================     >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
+echo. W-70 이벤트 로그 관리 설정
+echo.
+echo [ W-70 "이벤트 로그 관리 설정" ]
+echo.
+echo. # 응용프로그램 확인 #                                                                          
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Application" | find /I "MaxSize"     
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Application" | find /I "Retention"   
+echo. # 시스템 확인 #                                                                               
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\System" | find /I "MaxSize"          
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\System" | find /I "Retention"       
+echo. # 보안 확인 #                                                                               
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Security" | find /I "MaxSize"        
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\Eventlog\Security" | find /I "Retention" 
+echo.
+if errorlevel 1 (
+    echo ■ 결과: 양호
+    
+) else (
+    echo ■ 결과: 취약
+    echo ■ 조치 방안
+    echo Step 1 시작 - 실행 - EVENTVWR.MSC - 해당 로그 - 속성 - 일반
+    echo Step 2 최대 로그 크기 → 10240KB
+)
--- 배치_조사/조혜선_W58_W82/로그관리/71.bat
+++ 배치_조사/조혜선_W58_W82/로그관리/71.bat
@@ -1,15 +1,18 @@
 @echo off
 
-echo [w71] 원격에서 이벤트 로그 파일 접근 차단                                                    
-
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo [ W-71 "원격에서 이벤트 로그 파일 접근 차단 " ]                                                     >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-icacls %systemroot%\system32\Winevt\Logs | find /v "파일을 처리했으며"                               >>  [RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-icacls %systemroot%\system32\config | find /v "파일을 처리했으며" 									>>	[RESULT]_%COMPUTERNAME%_WINSVR.txt 2>&1
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo W-71 END                                                                                   >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo ======================================================================================     >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
-echo.                                                                                           >>	[RESULT]_%COMPUTERNAME%_WINSVR.txt
+echo. W-71 원격에서 이벤트 로그 파일 접근 차단
+echo.
+echo [ W-71 "원격에서 이벤트 로그 파일 접근 차단 " ]
+echo.
+icacls %systemroot%\system32\Winevt\Logs | find /v "파일을 처리했으며"                             
+icacls %systemroot%\system32\config | find /v "파일을 처리했으며"  
+echo.
+if errorlevel 1 (
+    echo ■ 결과: 양호
+    
+) else (
+    echo ■ 결과: 취약
+    echo ■ 조치 방안
+    echo Step 1 탐색기 - 로그 디렉터리 - 속성 - 보안
+    echo Step 2 Everyone 권한 제거
+)
