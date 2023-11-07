34fe94a800f19b36caec38028104fc6cc78a8757
gPtjs0420 <134833277+gPtjs0420@users.noreply.github.com>
2023-11-07 15:56:43 +0900

    Add files via upload

--- 배치_조사/조혜선_W58_W82/서비스관리/63.bat
+++ 배치_조사/조혜선_W58_W82/서비스관리/63.bat
@@ -1,20 +1,13 @@
 @echo off
-
 echo. W-63 DNS 서비스 구동 점검
 echo.
 echo [ W-63 "DNS 서비스 구동 점검" ]
 echo.
-net start | find /I "DNS Server"                                 
-net start | find /I "DNS Server"                                   
-net start | find /I "DNS Client"                            
-net start | find /I "DNS Client"
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\DNS Server\Zones\dx" | find /v 1
 if errorlevel 1 (
-    echo ■ 결과: 양호
-    
+echo ■ 결과: 양호
 ) else (
-    echo ■ 결과: 취약
-    echo ■ 조치 방안
-    echo Step 1 시작 - 실행 - DNSMGMT.MSC - 각 조회 영역 - 해당 영역 - 속성 - 일반
-    echo Step 2 동적 업데이트 → 없음 또는 아니오 선택
-    echo Step 3 불필요 시 해당 서비스 제거 시작 - 실행 - SERVICES.MSC - DNS 서버 - 속성 [일반] 탭에서 "시작 유형"을 "사용 안 함"으로 설정한 후, DNS 서비스 중지
+echo ■ 결과: 취약
+echo ■ 조치 방안
+echo Step 1 DNS 관리자'dnsmgmt.msc' - 각 조회 영역 - 속성 - '동적 업데이트' 설정 확인
 )
