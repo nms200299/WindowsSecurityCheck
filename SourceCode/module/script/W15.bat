ba410d6df1fbea3b30c0067f1286b17431d2ac41
nms200299 <nms200299@naver.com>
2023-11-23 10:56:50 +0900

    W09 추가

--- SourceCode/module/script/W15.bat
+++ SourceCode/module/script/W15.bat
@@ -35,10 +35,10 @@ if exist "C:\inetpub\" (
 	echo.
 	echo.
 	echo ■ 조치 방안
-	echo    1. 시작 -^> 제어판 -^> 관리도구 -^> 컴퓨터 관리 -^> 로컬 사용자 및 그룹 -^> 사용자 선택 -^> nobody 계정 추가 
-	echo    2. 시작 -^> 제어판 -^> 관리도구 -^> 로컬 보안 정책 -^> 사용자 권한 할당 선택, " 서비스 로그온" 에 "nobody" 계정 추가
-	echo    3. 시작 -^> 실행 -^> SERVICES.MSC -^> IIS Admin Service -^> 속성 -^> [로그온] 탭의 계정 지정에 nobody 계정 및 패스워드
-	echo    4. 시작 -^> 프로그램 -^> 윈도우 탐색기 -^> IIS가 설치된 폴더 속성 -^> [보안] 탭에서 nobody 계정을 추가하고 모든 권한 체크
+	echo    Step1^) 시작 -^> 제어판 -^> 관리도구 -^> 컴퓨터 관리 -^> 로컬 사용자 및 그룹 -^> 사용자 선택 -^> nobody 계정 추가 
+	echo    Step2^) 시작 -^> 제어판 -^> 관리도구 -^> 로컬 보안 정책 -^> 사용자 권한 할당 선택, " 서비스 로그온" 에 "nobody" 계정 추가
+	echo    Step3^) 시작 -^> 실행 -^> SERVICES.MSC -^> IIS Admin Service -^> 속성 -^> [로그온] 탭의 계정 지정에 nobody 계정 및 패스워드
+	echo    Step4^) 시작 -^> 프로그램 -^> 윈도우 탐색기 -^> IIS가 설치된 폴더 속성 -^> [보안] 탭에서 nobody 계정을 추가하고 모든 권한 체크
 	call %CHK_FILE% WARN %1
 	goto CONTINUE
 
--- SourceCode/module/script/W16.bat
+++ SourceCode/module/script/W16.bat
@@ -41,12 +41,14 @@ if exist %dirPath% (
         echo    Step1^) 제어판 -^> 시스템 및 보안 -^> 관리도구 -^> IIS관리자 -^>
         echo            해당 웹사이트 -^> 기본 설정 -^> "실제 경로"에서 홈 디렉토리 위치 확인
         echo    Step2^) 실제 경로에 입력된 홈 디렉토리로 이동하여 바로가기 파일을 삭제
-
+        call %CHK_FILE% PWN %1
     ) else (
         echo    → 양호 ^(심볼릭 링크, aliases, 바로가기 등을 사용하지 않음^)
+        call %CHK_FILE% SAFE %1
     )
 ) else (
     echo    → 양호 ^(IIS 웹 서비스를 사용하지 않음^)
+    call %CHK_FILE% SAFE %1
 )
 
 echo.
